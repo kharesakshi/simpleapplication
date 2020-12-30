198da5bc52b14d9fe50cf32b51b8cf615141991d
kharesakshi <sakshikhare1902@gmail.com>
2020-12-30 19:51:59 +0530

    Admin and Employee Login

---
+++ SimpleApplication/src/java/simpleapplication/model/Admin.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpleapplication.model;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Admin {
+    private int adminId;
+    private String password;
+    
+    public Admin(){
+        
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
---
+++ SimpleApplication/web/login-homepage.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : login-homepage
+    Created on : 30 Dec, 2020, 10:27:27 AM
+    Author     : Lenovo
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="styles/login.css">
+        <title>Login</title>
+    </head>
+    <body>
+        <div align="center">
+            <h3>Login as:</h3>
+                
+                <a href="admin-login.jsp"><button>ADMIN</button></a>
+                <a href="employee-login.jsp"><button>EMPLOYEE</button></a>
+        </div>
+    </body>
+</html>
